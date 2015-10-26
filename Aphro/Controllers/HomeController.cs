f13b44ff571edd0f7c606cbddf6aabd3b2085fe7
Scott Brady <scott@prolio.com>
2015-10-26 18:16:29 -0500

    Adding Amazon Database Connection

--- Aphro/Controllers/HomeController.cs
+++ Aphro/Controllers/HomeController.cs
@@ -1,11 +1,18 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Web.Mvc;
+using Oracle.DataAccess.Client;
 
-namespace Aprho.Controllers
+namespace Aphro.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            
+            OracleConnection myConnection = new OracleConnection();
+            myConnection.ConnectionString = ConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString;
+            myConnection.Open();
+            myConnection.Close();
             return View();
         }
     }
