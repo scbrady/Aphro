4a39f18528503ceb494a286930752ae839ac3f36
Scott Brady <scott@prolio.com>
2015-10-26 19:23:25 -0500

    Added Guest Controller

---
+++ Aphro/Controllers/GuestController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Aphro.Controllers
+{
+    public class GuestController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
