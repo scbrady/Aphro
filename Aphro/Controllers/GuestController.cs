ad68f9033f6905bb00619c87f209d6be6ed1efb4
Scott Brady <scott@prolio.com>
2015-10-27 20:53:11 -0500

    Got DB working
    Able to register a guest account now

--- Aphro/Controllers/GuestController.cs
+++ Aphro/Controllers/GuestController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using Aphro.Models;
 
 namespace Aphro.Controllers
 {
@@ -7,6 +8,28 @@ namespace Aphro.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(guests guest)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DataModelContainer dc = new DataModelContainer())
+                {
+                    dc.guests.Add(guest);
+                    dc.SaveChanges();
+                    ModelState.Clear();
+                }
+                return RedirectToAction("Index");
+            }
+            return View(guest);
         }
     }
 }
